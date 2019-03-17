e38c3ca3f7c10b73f2af44b14798b736ae2762ec
shenjl <selonsy@gmail.com>
2019-03-17 09:53:11 +0800

    update

--- myTest.py
+++ myTest.py
@@ -3,3 +3,34 @@ sess = tf.Session()
 a = tf.constant(1) 
 b = tf.constant(2) 
 print(sess.run(a+b)) 
+
+
+import numpy as np
+a = np.repeat(np.arange(5).reshape([1,-1]),10,axis = 0)+10.0 
+b = np.random.randint(5, size= a.shape)
+
+{
+    "version": "0.1.0",
+    "command": "g++",
+    "args": [
+        "-g",
+        "${file}",
+        "-o",
+        "${file}.exe"
+    ],
+    "problemMatcher": {
+        "owner": "cpp",
+        "fileLocation": [
+            "relative",
+            "${workspaceRoot}"
+        ],
+        "pattern": {
+            "regexp": "^(.*):(\\d+):(\\d+):\\s+(warning|error):\\s+(.*)$",
+            "file": 1,
+            "line": 2,
+            "column": 3,
+            "severity": 4,
+            "message": 5
+        }
+    }
+}
