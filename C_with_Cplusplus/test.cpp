7b39537f22ab5025d51151ac52f9cf0393179eda
shenjl <selonsy@gmail.com>
2019-03-17 09:50:32 +0800

    add cpp

---
+++ C_with_Cplusplus/test.cpp
@@ -0,0 +1,22 @@
+#include <iostream>
+#include <string>
+using namespace std;
+
+void rev_str(char *s,int len)
+{
+    char ch;
+    if ( len > 1)
+        {
+             ch = *s;
+             *s = *(s+len-1);
+             *(s+len-1) = ch;
+             rev_str(s+1,len-2);
+        }
+}
+int main()
+{
+    char s[]= "uvxyz";
+    rev_str(s,5);
+    cout << s << endl;
+}
+
