797ab3317cb86ee20aa44b31e96315dc88a9291e
shenjl <selonsy@gmail.com>
2019-05-08 19:22:45 +0800

    update.

--- Python3.6/aim2offer/LeetCode/62.不同路径.py
+++ Python3.6/aim2offer/LeetCode/62.不同路径.py
@@ -1,15 +1,43 @@
+'''
+一个机器人位于一个 m x n 网格的左上角 （起始点在下图中标记为“Start” ）。
+机器人每次只能向下或者向右移动一步。机器人试图达到网格的右下角（在下图中标记为“Finish”）。
+问总共有多少条不同的路径？
+例如，上图是一个7 x 3 的网格。有多少可能的路径？
+
+说明：m 和 n 的值均不超过 100。
+
+示例 1:
+输入: m = 3, n = 2
+输出: 3
+解释:
+从左上角开始，总共有 3 条路径可以到达右下角。
+1. 向右 -> 向右 -> 向下
+2. 向右 -> 向下 -> 向右
+3. 向下 -> 向右 -> 向右
+
+示例 2:
+输入: m = 7, n = 3
+输出: 28
+'''
 import time
 import copy
-class Solution:    
-    def func1(self, x):
-        pass
+class Solution:   
+    # def uniquePaths(self, m: int, n: int) -> int: 
+    def func1(self, m, n):
+        if n == 1: return 1
+        if n == 2: return m
+        if n == 3: return m*(1+m)/2
+        res = 0
+        for i in range(n-1,0,-1):
+            res += i * Solution.func1(self, i, n-1)
+        return res
      
     def getVarLen(self):
         co = self.func1.__code__        
         return co.co_argcount
 
-ori_data = [1]
-expect_data = [None]
+ori_data = [[3,2],[7,3],[3,7]]
+expect_data = [3,28,28]
 assert len(ori_data) == len(expect_data),'输入输出数据数量不一致'
 
 s = Solution()
--- Python3.6/aim2offer/LeetCode/930. 和相同的二元子数组.py
+++ Python3.6/aim2offer/LeetCode/930. 和相同的二元子数组.py
@@ -1,15 +1,73 @@
+'''
+在由若干 0 和 1 组成的数组 A 中，有多少个和为 S 的非空子数组。
+
+示例：
+输入：A = [1,0,1,0,1], S = 2
+输出：4
+解释：
+如下面黑体(加引号表示)所示，有 4 个满足题目要求的子数组：
+['1,0,1',0,1]
+['1,0,1,0',1]
+[1,'0,1,0,1']
+[1,0,'1,0,1']
+
+提示：
+    A.length <= 30000
+    0 <= S <= A.length
+    A[i] 为 0 或 1
+'''
 import time
 import copy
 class Solution:    
-    def func1(self, x):
-        pass
-     
+    # def numSubarraysWithSum(self, A: List[int], S: int) -> int:
+    def func1(self, A, S): # 时间复杂度o(n^2) 超时了 53 / 59 
+        l = len(A)
+        res = 0
+        _sum = 0          
+        for i in range(l):                          
+            for j in range(i,l):
+                _sum += A[j]
+                if _sum == S:
+                    res += 1
+                if _sum > S:                   
+                    break  
+            _sum = 0              
+        return res
+    def func2(self, A, S):
+        P = [0]
+        for x in A: 
+            P.append(P[-1] + x)
+        import collections
+        count = collections.Counter()
+
+        ans = 0
+        for x in P:
+            ans += count[x]
+            count[x + S] += 1
+
+        return ans
+    def func3(self, A, S):
+        indexes = [-1] + [ix for ix, v in enumerate(A) if v] + [len(A)]
+        ans = 0
+        if S == 0:
+            for i in range(len(indexes) - 1):
+                # w: number of zeros between two consecutive ones
+                w = indexes[i+1] - indexes[i] - 1
+                ans += w * (w+1) / 2  # n个连续0中选长度大于等于1的连续子串数量
+            return ans
+        for i in range(1, len(indexes) - S):
+            j = i + S - 1
+            left = indexes[i] - indexes[i-1]
+            right = indexes[j+1] - indexes[j]
+            ans += left * right
+        return ans
+    
     def getVarLen(self):
         co = self.func1.__code__        
         return co.co_argcount
 
-ori_data = [1]
-expect_data = [None]
+ori_data = [[[1,0,1,0,1],2],[[0,0,0,0,1],2]]
+expect_data = [4,0]
 assert len(ori_data) == len(expect_data),'输入输出数据数量不一致'
 
 s = Solution()
