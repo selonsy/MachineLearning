ed4cd6ee9c6bd55f4a6b17b5e0a7b2bdca54343f
shenjl <selonsy@gmail.com>
2019-11-10 18:15:25 +0800

    update.

--- Python3.6/Python3.6.py
+++
@@ -1,8 +0,0 @@
-print('Hello world!')
-
-a = 3
-b = 4
-c = a + b
-
-print(c)
-# this is a test.
--- Python3.6/临时待整理/main.py
+++
@@ -1,8 +0,0 @@
-# -*- coding: utf-8 -*-
-import numpy as np
-
-w = [1,2,3,4,5,2]
-w = np.unique(w)
-print(type(w))
-
-#print('Hello World!','沈金龙1')
